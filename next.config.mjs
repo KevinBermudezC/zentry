007b4d083549270be9eef4e30e7b5b2a02ed12de
KevinBermudezC <kevinbermudezc@gmail.com>
2024-11-24 23:36:04 -0500

    :ambulance: Update nextconfig for production again

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  Eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
