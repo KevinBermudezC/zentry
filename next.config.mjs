03258f7efb8ea21460c53bf5acde513b08849e40
KevinBermudezC <kevinbermudezc@gmail.com>
2024-11-24 23:32:33 -0500

    :ambulance: Update nextconfig for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      // Disable ESLint for production builds
+      config.module.rules = config.module.rules.filter(
+        (rule) => !(rule.use && rule.use.loader === "eslint-loader"),
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
