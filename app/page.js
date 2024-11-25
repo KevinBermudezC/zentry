e24ce9e373fbd045bb0cc4c8eefd0392a0db08b4
KevinBermudezC <kevinbermudezc@gmail.com>
2024-11-24 21:09:59 -0500

    :art: Hero done and about sections

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+    </main>
+  );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+import React, { useRef } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+import Button from "@/components/Button";
+
+const Navbar = () => {
+  const navContainerRef = useRef(null);
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/img/logo.png" alt="logo" className="w-10" />
+
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              className="hidden items-center justify-center gap-1 bg-blue-50 md:flex"
+            />
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+export default Navbar;
